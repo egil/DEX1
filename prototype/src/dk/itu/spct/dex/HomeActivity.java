5e6f5b8d45777bfe5b5f5aa38e6cbce6ff9c999a
David Thomas <davidat@gmail.com>
2012-04-03 18:57:51 +0200

    Initial android project

---
+++ prototype/src/dk/itu/spct/dex/HomeActivity.java
@@ -0,0 +1,14 @@
+package dk.itu.spct.dex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+  
+}
