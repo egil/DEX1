71e6ddffaa2f32b0b26827328bd70147e266413d
David Thomas <davidat@gmail.com>
2012-05-02 16:04:11 +0200

    app icon

--- prototype/gen/dk/itu/spcl/dex/R.java
+++ prototype/gen/dk/itu/spcl/dex/R.java
@@ -11,14 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_tab_default=0x7f020001;
-        public static final int off=0x7f020002;
-        public static final int off_check=0x7f020003;
-        public static final int off_nocheck=0x7f020004;
-        public static final int on=0x7f020005;
-        public static final int on_check=0x7f020006;
-        public static final int on_nocheck=0x7f020007;
+        public static final int home_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_tab_default=0x7f020002;
+        public static final int off=0x7f020003;
+        public static final int off_check=0x7f020004;
+        public static final int off_nocheck=0x7f020005;
+        public static final int on=0x7f020006;
+        public static final int on_check=0x7f020007;
+        public static final int on_nocheck=0x7f020008;
     }
     public static final class id {
         public static final int addThingyMenu=0x7f060005;
